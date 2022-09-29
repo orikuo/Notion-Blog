7677dc9204ac1d7ab219468b832ed3b7e87f186f
orikuo <72489432+orikuo@users.noreply.github.com>
2022-09-29 22:49:12 +0800

    🌈 Initial

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "next", "standard"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/prop-types": "off",
+    "comma-dangle": "off",
+    semi: "off",
+    quotes: "off",
+    "multiline-ternary": "off",
+    "space-before-function-paren": "off",
+  },
+  globals: {
+    React: true,
+  },
+};
